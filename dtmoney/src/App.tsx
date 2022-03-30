5737bce233ed14074bd83a7fcc1e0e926fde85bd
Guilherme Zobel <guilherme.zobel@gmail.com>
2022-03-30 12:11:12 -0300

    Feat: Install React Modal & component New Transaction Model

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,12 +1,34 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from './components/NewTransactionModal';
+
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement('#root')
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function hanleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function hanleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={hanleOpenNewTransactionModal}/>
+
       <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={hanleCloseNewTransactionModal}/>
+
       <GlobalStyle />
     </>
   );
