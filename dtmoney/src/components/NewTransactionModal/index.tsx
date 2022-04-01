caa1ae67c21c254487d518c8c96e26b33f61c088
Guilherme Zobel <guilherme.zobel@gmail.com>
2022-03-31 22:19:09 -0300

    Style: Creating type controls

--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal'
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
 import closeImg from '../../assets/close.svg';
-import { Cointainer } from './style';
+import { Cointainer, TransactionTypeContainer } from './style';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -35,7 +37,21 @@ export function NewTransactionModal({ isOpen, onRequestClose}: NewTransactionMod
               placeholder='Valor'
             />
 
+            <TransactionTypeContainer>
+              <button
+                type="button"
+              >
+                <img src={incomeImg} alt="Entrada" />
+                <span>Entrada</span>
+              </button>
 
+              <button
+                type="button"
+              >
+                <img src={outcomeImg} alt="Saída" />
+                <span>Saída</span>
+              </button>
+            </TransactionTypeContainer>
 
             <input 
               placeholder='Categoria'
