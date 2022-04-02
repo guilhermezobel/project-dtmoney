7f83fd33a81fdbdbed79c8b48f0def22432127b6
Guilherme Zobel <guilherme.zobel@gmail.com>
2022-04-01 23:40:19 -0300

    Feat: Saving form states

--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -3,7 +3,7 @@ import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import closeImg from '../../assets/close.svg';
 import { Cointainer, RadioBox, TransactionTypeContainer } from './style';
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -11,7 +11,21 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose}: NewTransactionModalProps) {
+  const [title, setTitle] = useState('')
+  const [value, setValue] = useState(0)
+  const [category, setCategory] = useState('')
   const [type, setType] = useState('deposit')
+
+  function handleCreateNewTransaction(event: FormEvent) {
+    event.preventDefault();
+    console.log(
+      title,
+      value,
+      category,
+      type,
+    );
+    
+  };
 
   return (
     <Modal
@@ -28,16 +42,18 @@ export function NewTransactionModal({ isOpen, onRequestClose}: NewTransactionMod
             <img src={closeImg} alt="Fechar modal" />
           </button>
 
-          <Cointainer>
+          <Cointainer onSubmit={handleCreateNewTransaction}>
             <h2>Cadastrar Transação</h2>
 
             <input
               placeholder='Título'
+              onChange={event => setTitle(event.target.value)}
             />
 
             <input
               type="number"
               placeholder='Valor'
+              onChange={event => setValue (Number(event.target.value))}
             />
 
             <TransactionTypeContainer>
@@ -64,6 +80,7 @@ export function NewTransactionModal({ isOpen, onRequestClose}: NewTransactionMod
 
             <input 
               placeholder='Categoria'
+              onChange={event => setCategory(event.target.value)}
             />
 
             <button type="submit">
@@ -73,5 +90,6 @@ export function NewTransactionModal({ isOpen, onRequestClose}: NewTransactionMod
 
           </Cointainer>
         </Modal>
-  );
+
+);
 }
