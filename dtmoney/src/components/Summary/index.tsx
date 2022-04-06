ce4cfea4fee162747092d2cab38a8b1cdcc79d2e
Guilherme Zobel <guilherme.zobel@gmail.com>
2022-04-06 11:35:10 -0300

    Feat: calculating dashboard

--- dtmoney/src/components/Summary/index.tsx
+++ dtmoney/src/components/Summary/index.tsx
@@ -9,7 +9,22 @@ import { Contnainer } from "./style";
 export function Summary() {
   const { transactions } = useContext(TransactionsContext);
 
-  console.log(transactions);
+ const summary = transactions.reduce((acc, transaction) => {
+   if (transaction.type === 'deposit') {
+     acc.deposits += transaction.amount;
+     acc.total += transaction.amount;
+   } else {
+     acc.withdraws += transaction.amount;
+     acc.total -= transaction.amount;
+   }
+
+   return acc;
+ }, {
+   deposits: 0,
+   withdraws: 0,
+   total: 0
+ });
+
   
   return(
     <Contnainer>
@@ -18,20 +33,36 @@ export function Summary() {
         <p>Entradas</p>
         <img src={icomeImg} alt="Entradas" />
        </header>
-      <strong>R$1000,00</strong>
-     </div>     <div>
+        <strong>
+        {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.deposits)}
+        </strong>
+     </div>
+     <div>
        <header>
         <p>Entradas</p>
         <img src={oucomeImg} alt="Saídas" />
        </header>
-      <strong>- R$500,00</strong>
+      <strong>-
+      {new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+      }).format(summary.withdraws)}
+        </strong>
      </div>
      <div className="highlight-background">
        <header>
         <p>Entradas</p>
         <img src={totalImg} alt="Entradas" />
        </header>
-      <strong>R$500,00</strong>
+      <strong>
+      {new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+      }).format(summary.total)}
+      </strong>
      </div>
     </Contnainer>
   );
